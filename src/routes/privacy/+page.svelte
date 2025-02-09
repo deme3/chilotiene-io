63ae4ea61bb9c5b0f68fb54b656fb60f9ccc7c0d
Demetrio Battaglia <deme3.iot@gmail.com>
2025-02-09 21:15:41 +0100

    feat: add privacy policy page

---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+	import { goto } from '$app/navigation';
+	import { base } from '$app/paths';
+	import Accordion from '$lib/components/Accordion.svelte';
+</script>
+
+<div id="page-header" class="flex min-h-16 flex-row items-center justify-between gap-4">
+	<div>
+		<h1 class="small-logo font-bold text-zinc-200/50" id="logo">Chi lo tiene...</h1>
+		<h2 class="text-xl font-bold"><strong>Privacy Policy</strong></h2>
+	</div>
+	<div class="flex items-center justify-end gap-4">
+		<button class="circular-button" aria-label="Go Home" onclick={() => goto(`${base}/`)}>
+			<i class="ti ti-home"></i>
+		</button>
+		<button class="circular-button" aria-label="Go Back" onclick={() => history.back()}>
+			<i class="ti ti-arrow-left"></i>
+		</button>
+	</div>
+</div>
+
+<section class="mt-4 flex flex-col gap-4">
+	<Accordion title="Privacy Policy" noncollapsible>
+		<article class="prose prose-base prose-invert !max-w-full px-2 prose-headings:mb-0">
+			<h4>WIP</h4>
+			<p></p>
+		</article>
+	</Accordion>
+</section>
